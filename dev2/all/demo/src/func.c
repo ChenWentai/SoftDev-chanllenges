0df57353b86c037e913c9e83db4de134cb59ac86
CHEN Wentai <cwentai01@gmail.com>
2017-02-28 14:58:58 +0100

    Add files via upload

---
+++ dev2/all/demo/src/func.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <func.h>
+void f(int x, int y, int z){
+	printf("%d\n",x+y+z);
+}
---
+++ dev2/all/project2/config.h
@@ -0,0 +1,32 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* CONSTANT THAT ALLOWS TO SELECT THE RIGHT FUNCTION CALL */
+#define FAKE_MISTERY_VALUE_ONEPARAM /**/
+
+/* CONSTANT THAT ALLOWS TO SELECT THE RIGHT FUNCTION CALL */
+/* #undef MISTERY_VALUE_ONEPARAM */
+
+/* Name of package */
+#define PACKAGE "project"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "maci@eurecom.fr"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "project"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "project 1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "project"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1"
+
+/* Version number of package */
+#define VERSION "1"
